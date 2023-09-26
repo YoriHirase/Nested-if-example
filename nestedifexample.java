95d9ccd0468a608425d367a1ff58d3b5b0bac3f0
Martino <143937413+YoriHirase@users.noreply.github.com>
2023-09-27 03:22:00 +0800

    Update nestedifexample.java

--- nestedifexample.java
+++ nestedifexample.java
@@ -2,55 +2,47 @@ import java.util.Scanner;
 
 public class nestedifexample {
     public static void main(String[] args) {
-        Scanner scan = new Scanner (System.in);
-        int laptop, buku, kotak, smartphone;
-        Double berat, beratLaptop, beratbuku, beratkotak, beratsmartphone;
+        Scanner scan = new Scanner(System.in);
+        int barang,hadiah,cashback, jumlahbarang;
+         
         
-        System.out.println("======== Indeks Berat Barang Bawaan Tasku ========");
-        System.out.print("Banyak laptop: ");
-        laptop = scan.nextInt();
-        System.out.print("Banyak buku tulis: ");
-        buku = scan.nextInt();
-        System.out.print("Banyak kotak pensil: ");
-        kotak = scan.nextInt();
-        System.out.print("Banyak smartphone: ");
-        smartphone = scan.nextInt();
-        System.out.println("==================================================");
-        
-        beratLaptop = laptop*850.56;
-        beratbuku = buku*250.11;
-        beratkotak = kotak*25.31;
-        beratsmartphone = smartphone*200.00;
-        berat = beratLaptop + beratbuku + beratkotak + beratsmartphone;
-        
-        if(laptop <= 0 || buku <= 0 || kotak <= 0 || smartphone <= 0){
-            System.out.println("ERROR - Tidak diperkenankan bilangan negatif!");
-        }
-        else if(berat < 2000){
-            System.out.println("Indeks Berat Barang: 5");   
-        }
-        else if (berat > 1500 && berat < 2000){
-            System.out.println("Indeks Berat Barang: 4");
-        }
-        else if (berat > 1000 && berat < 1500){
-            System.out.println("Indeks Berat Barang: 3");
-        }
-        else if (berat > 500 && berat < 1000){
-            System.out.println("Indeks Berat Barang: 2");
-        }
-        else if (berat > 0 && berat < 500){
-            System.out.println("Indeks Berat Barang: 1");
-        }
-        else if (berat == 0){
-            System.out.println("Indeks Berat Barang: 0");
-        }
+        System.out.println("=====Selamat Datang di Toko Charlie=====");
+        System.out.print("Jumlah beli barang: ");
+        barang = scan.nextInt();
+        System.out.println("========================================");
        
-        else if(berat <= 2000){
-            System.out.println("Berat bawaan tidak melebihi ketentuan");
-        }
-        else if(berat > 2000){
-            System.out.println("Berat barang melebihi ketentuan");
-        }
+        
+        hadiah = barang / 3;
+        cashback = barang / 7;
+        jumlahbarang = hadiah + barang;
+        
+         if (barang <= 0) {
+            System.out.println("ERROR - Inputan harus lebih dari 0");
+            
+         } else{
+            System.out.print("Cashback: Rp");
+             if (barang % 7 == 0){
+            System.out.println(cashback * 5500);
+            }
+             else if (barang < 7)
+                 System.out.println("-");
+             else {
+            System.out.println(cashback*5500);
+            }
+        
+            System.out.print("Tambahan Barang: ");
+            if (barang % 3 == 0){
+            System.out.println(hadiah*1);
+             }
+            else if (barang < 3){
+            System.out.println("-");
+            }
+            else{
+                System.out.println(hadiah*1);
+            }
+            System.out.println("Jumlah Barang: " + jumlahbarang);
+         }
+    
         
     }
     
